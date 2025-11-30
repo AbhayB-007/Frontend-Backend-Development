bcd5125526efa89494acc0494a160abef7c8b817
AbhayB-007 <abhaybindal523@gmail.com>
2025-11-30 23:21:23 +0530

    Add TypeScript files and configuration for initial setup

---
+++ Typescript/main.js
@@ -0,0 +1,24 @@
+function doSomething() {
+    var i1 = 0;
+    for (var i = 0; i < 5; i++) {
+        console.log(i);
+        i1 = i;
+    }
+    console.log('Finally i1: ' + i1);
+}
+doSomething();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Typescript/main.ts
@@ -0,0 +1,24 @@
+function doSomething() {
+    var i1: number = 0;
+    for (let i = 0; i < 5; i++) {
+        console.log(i);
+        i1 = i;
+    }
+    console.log('Finally i1: ' + i1);
+}
+doSomething();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
